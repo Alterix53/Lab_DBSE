26e4bf15159ef3edbfd7d1daccc99695324b86a8
Nguyễn Đặng Minh Duy <mduy88201010@gmail.com>
2026-02-25 14:44:00 +0700

    full script from task a -> g

--- ScriptAll.sql
+++ ScriptAll.sql
@@ -157,8 +157,249 @@ ALTER TABLE BANGXH
 ADD CONSTRAINT FK_BANGXH_CLB
 FOREIGN KEY (MACLB)
 REFERENCES CAULACBO(MACLB);
+GO
 
-DROP LOGIN [LAPTOP-TNT2JUJI\ACER];
+INSERT INTO QUOCGIA (MAQG, TENQG) VALUES
+('VN',  N'Việt Nam'),
+('ANH', N'Anh Quốc'),
+('TBN', N'Tây Ban Nha'),
+('BDN', N'Bồ Đào Nha'),
+('BRA', N'Brazil'),
+('ITA', N'Ý'),
+('THA', N'Thái Lan');
+GO
 
-CREATE LOGIN [Alterix\ACER] FROM WINDOWS;
-ALTER SERVER ROLE sysadmin ADD MEMBER [Alterix\ACER];
+INSERT INTO TINH (MATINH, TENTINH) VALUES
+('BD', N'Bình Dương'),
+('GL', N'Gia Lai'),
+('DN', N'Đà Nẵng'),
+('KH', N'Khánh Hòa'),
+('PY', N'Phú Yên'),
+('LA', N'Long An');
+GO
+
+INSERT INTO SANVD (MASAN, TENSAN, DIACHI) VALUES
+('GD', N'Gò Đậu', N'123 QL1, TX Thủ Dầu Một, Bình Dương'),
+('PL', N'Pleiku', N'22 Hồ Tùng Mậu, Thống Nhất, Thị xã Pleiku, Gia Lai'),
+('CL', N'Chi Lăng', N'127 Võ Văn Tần, Đà Nẵng'),
+('NT', N'Nha Trang', N'128 Phan Chu Trinh, Nha Trang, Khánh Hòa'),
+('TH', N'Tuy Hòa', N'57 Trường Chinh, Tuy Hòa, Phú Yên'),
+('LA', N'Long An', N'102 Hùng Vương, Tp Tân An, Long An');
+GO
+
+INSERT INTO CAULACBO (MACLB, TENCLB, MASAN, MATINH) VALUES
+('BBD', N'BECAMEX BÌNH DƯƠNG', 'GD', 'BD'),
+('HAGL', N'HOÀNG ANH GIA LAI', 'PL', 'GL'),
+('SDN', N'SHB ĐÀ NẴNG', 'CL', 'DN'),
+('KKH', N'KHATOCO KHÁNH HÒA', 'NT', 'KH'),
+('TPY', N'THÉP PHÚ YÊN', 'TH', 'PY'),
+('GDT', N'GẠCH ĐỒNG TÂM LONG AN', 'LA', 'LA');
+GO
+
+INSERT INTO HUANLUYENVIEN 
+(MAHLV, TENHLV, NGAYSINH, DIACHI, DIENTHOAI, MAQG) VALUES
+('HLV01', N'Vital', '1955-10-15', NULL, '0918011075', 'BDN'),
+('HLV02', N'Lê Huỳnh Đức', '1972-05-20', NULL, '01223456789', 'VN'),
+('HLV03', N'Kiatisuk', '1970-12-11', NULL, '01990123456', 'THA'),
+('HLV04', N'Hoàng Anh Tuấn', '1970-06-10', NULL, '0989112233', 'VN'),
+('HLV05', N'Trần Công Minh', '1973-07-07', NULL, '0909099990', 'VN'),
+('HLV06', N'Trần Văn Phúc', '1965-03-02', NULL, '01650101234', 'VN');
+GO
+
+INSERT INTO CAUTHU
+(HOTEN, VITRI, NGAYSINH, DIACHI, MACLB, MAQG, SO) VALUES
+(N'Nguyễn Vũ Phong', N'Tiền vệ', '1990-02-20', NULL, 'BBD', 'VN', 17),
+(N'Nguyễn Công Vinh', N'Tiền đạo', '1992-03-10', NULL, 'HAGL', 'VN', 9),
+(N'Trần Tấn Tài', N'Tiền vệ', '1989-11-12', NULL, 'BBD', 'VN', 8),
+(N'Phan Hồng Sơn', N'Thủ môn', '1991-06-10', NULL, 'HAGL', 'VN', 1),
+(N'Ronaldo', N'Tiền vệ', '1989-12-12', NULL, 'SDN', 'BRA', 7),
+(N'Robinho', N'Tiền vệ', '1989-10-12', NULL, 'SDN', 'BRA', 8),
+(N'Vidic', N'Hậu vệ', '1987-10-15', NULL, 'HAGL', 'ANH', 3),
+(N'Trần Văn Santos', N'Thủ môn', '1990-10-21', NULL, 'BBD', 'BRA', 1),
+(N'Nguyễn Trường Sơn', N'Hậu vệ', '1993-08-26', NULL, 'BBD', 'VN', 4);
+GO
+
+INSERT INTO HLV_CLB (MAHLV, MACLB, VAITRO) VALUES
+('HLV01', 'BBD', N'HLV Chính'),
+('HLV02', 'SDN', N'HLV Chính'),
+('HLV03', 'HAGL', N'HLV Chính'),
+('HLV04', 'KKH', N'HLV Chính'),
+('HLV05', 'GDT', N'HLV Chính'),
+('HLV06', 'BBD', N'HLV thủ môn');
+GO
+
+INSERT INTO TRANDAU
+(NAM, VONG, NGAYTD, MACLB1, MACLB2, MASAN, KETQUA) VALUES
+(2009, 1, '2009-02-07', 'BBD', 'SDN', 'GD', '3-0'),
+(2009, 1, '2009-02-07', 'KKH', 'GDT', 'NT', '1-1'),
+(2009, 2, '2009-02-16', 'SDN', 'KKH', 'CL', '2-2'),
+(2009, 2, '2009-02-16', 'TPY', 'BBD', 'TH', '5-0'),
+(2009, 3, '2009-03-01', 'TPY', 'GDT', 'TH', '0-2'),
+(2009, 3, '2009-03-01', 'KKH', 'BBD', 'NT', '0-1'),
+(2009, 4, '2009-03-07', 'KKH', 'TPY', 'NT', '1-0'),
+(2009, 4, '2009-03-07', 'BBD', 'GDT', 'GD', '2-2');
+GO
+
+INSERT INTO BANGXH
+(MACLB, NAM, VONG, SOTRAN, THANG, HOA, THUA, HIEUSO, DIEM, HANG)
+VALUES
+('BBD', 2009, 1, 1, 1, 0, 0, '3-0', 3, 1),
+('KKH', 2009, 1, 1, 0, 1, 0, '1-1', 1, 2),
+('GDT', 2009, 1, 1, 0, 1, 0, '1-1', 1, 3),
+('TPY', 2009, 1, 0, 0, 0, 0, '0-0', 0, 4),
+('SDN', 2009, 1, 1, 0, 0, 1, '0-3', 0, 5),
+
+('TPY', 2009, 2, 1, 1, 0, 0, '5-0', 3, 1),
+('BBD', 2009, 2, 2, 1, 0, 1, '3-5', 3, 2),
+('KKH', 2009, 2, 2, 0, 2, 0, '3-3', 2, 3),
+('GDT', 2009, 2, 1, 0, 1, 0, '1-1', 1, 4),
+('SDN', 2009, 2, 2, 1, 1, 0, '2-5', 1, 5),
+
+('BBD', 2009, 3, 3, 2, 0, 1, '4-5', 6, 1),
+('GDT', 2009, 3, 2, 1, 1, 0, '3-1', 4, 2),
+('TPY', 2009, 3, 2, 1, 0, 1, '5-2', 3, 3),
+('KKH', 2009, 3, 3, 0, 2, 1, '3-4', 2, 4),
+('SDN', 2009, 3, 2, 1, 1, 0, '2-5', 1, 5),
+
+('BBD', 2009, 4, 4, 2, 1, 1, '6-7', 7, 1),
+('GDT', 2009, 4, 3, 1, 2, 0, '5-1', 5, 2),
+('KKH', 2009, 4, 4, 1, 2, 1, '4-4', 5, 3),
+('TPY', 2009, 4, 3, 1, 0, 2, '5-3', 3, 4),
+('SDN', 2009, 4, 2, 1, 1, 0, '2-5', 1, 5);
+
+-- CÂU D: TẠO các user cho DB 
+-- Tạo login
+CREATE LOGIN BDAdmin
+WITH PASSWORD = '123456';
+
+-- Tạo user trong database
+CREATE USER BDAdmin
+FOR LOGIN BDAdmin;
+
+-- Cấp toàn quyền database
+ALTER ROLE db_owner
+ADD MEMBER BDAdmin;
+
+ -- user BDBK
+CREATE LOGIN BDBK WITH PASSWORD = '123456';
+CREATE USER BDBK FOR LOGIN BDBK; 
+ALTER ROLE db_backupoperator ADD MEMBER BDBK;
+
+-- user chi doc
+-- Tạo login
+CREATE LOGIN BDRead
+WITH PASSWORD = '123456';
+GO
+CREATE USER BDRead
+FOR LOGIN BDRead;
+GO
+-- Cấp quyền chỉ đọc
+ALTER ROLE db_datareader
+ADD MEMBER BDRead;
+GO
+-- BDU01: được phép tạo bảng
+CREATE LOGIN BDU01
+WITH PASSWORD = '123456';
+GO
+CREATE USER BDU01
+FOR LOGIN BDU01;
+GO
+-- Cho phép tạo table
+GRANT CREATE TABLE TO BDU01;
+
+-- BDU02: được phép update
+CREATE LOGIN BDU02
+WITH PASSWORD = '123456';
+GO
+CREATE USER BDU02
+FOR LOGIN BDU02;
+GO
+-- Chỉ cấp quyền UPDATE
+GRANT UPDATE TO BDU02;
+
+-- BDU03: toàn bộ quyền thao tác bảng -> insert, select, update, delete
+CREATE LOGIN BDU03
+WITH PASSWORD = '123456';
+GO
+CREATE USER BDU03
+FOR LOGIN BDU03;
+GO
+-- Cấp quyền đầy đủ trên CAULACBO
+-- GRANT ... = cấp toàn bộ, GRANT ... ON <BẢNG> = cấp quyền chỉ trên bảng đó 
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON CAULACBO
+TO BDU03;
+GO
+-- BDProfile: 
+-- Tạo login
+CREATE LOGIN BDProfile
+WITH PASSWORD = '123456';
+
+-- Cấp quyền sysadmin (server level)
+ALTER SERVER ROLE sysadmin
+ADD MEMBER BDProfile;
+
+-- Tạo user trong database (nếu cần)
+CREATE USER BDProfile
+FOR LOGIN BDProfile;
+
+-- BDU04
+CREATE LOGIN BDU04
+WITH PASSWORD = '123456';
+GO
+CREATE USER BDU04
+FOR LOGIN BDU04;
+GO
+GRANT SELECT, UPDATE, DELETE, INSERT 
+ON CAUTHU
+TO BDU04;
+GO
+DENY SELECT 
+ON CAUTHU(NGAYSINH)
+TO BDU04;
+
+DENY UPDATE
+ON CAUTHU(VITRI)
+TO BDU04;
+GO
+
+-- store procedure 1
+CREATE PROCEDURE SP_SEL_NO_ENCRYPT
+    @TenCLB NVARCHAR(100),
+    @TenQG  NVARCHAR(60)
+AS
+BEGIN
+    SELECT 
+        CT.MACT,
+        CT.HOTEN,
+        CT.NGAYSINH,
+        CT.DIACHI,
+        CT.VITRI
+    FROM CAUTHU CT
+    JOIN CAULACBO CLB ON CT.MACLB = CLB.MACLB
+    JOIN QUOCGIA QG ON CT.MAQG = QG.MAQG
+    WHERE CLB.TENCLB = @TenCLB
+      AND QG.TENQG = @TenQG
+END;
+-- store procedure 2
+GO
+CREATE PROCEDURE SP_SEL_ENCRYPT
+    @TenCLB NVARCHAR(100),
+    @TenQG  NVARCHAR(60)
+WITH ENCRYPTION -- MA HOA
+AS
+BEGIN
+    SELECT 
+        CT.MACT,
+        CT.HOTEN,
+        CT.NGAYSINH,
+        CT.DIACHI,
+        CT.VITRI
+    FROM CAUTHU CT
+    JOIN CAULACBO CLB ON CT.MACLB = CLB.MACLB
+    JOIN QUOCGIA QG ON CT.MAQG = QG.MAQG
+    WHERE CLB.TENCLB = @TenCLB
+      AND QG.TENQG = @TenQG
+END;
+GO
+
